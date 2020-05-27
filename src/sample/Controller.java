a7030b009aeef32e4814f199eb958d92b3bedd72
elka74 <mikheeva@1454.ru>
2020-05-27 04:02:38 +0300

    ChatRoom

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,5 +7,20 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    
+    @FXML
+    public Button btn;
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    public void btnClick(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText());
+        textField.setText("");
+    }
+
+    public void textEnter(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText());
+        textField.setText("");
+    }
 }
